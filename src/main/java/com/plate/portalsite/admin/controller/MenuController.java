2915595ca27cea3febb73fe8fca7735c1a614cdc
xiongxiao <1342011173@qq.com>
2020-01-01 22:52:13 +0800

    优化菜单管理界面

--- src/main/java/com/plate/portalsite/admin/controller/MenuController.java
+++ src/main/java/com/plate/portalsite/admin/controller/MenuController.java
@@ -39,15 +39,41 @@ public class MenuController {
         if(StringUtils.isEmpty(menuItem.getParentId())){
             return;
         }
-        if(!StringUtils.isEmpty(menuItem.getData()))
-            menuItem.setData(menuItem.getData().substring(1,menuItem.getData().length()));
+//        if(!StringUtils.isEmpty(menuItem.getData()))
+//            menuItem.setData(menuItem.getData().substring(1,menuItem.getData().length()));
         menuService.saveOrUpdate(menuItem);
     }
+
+    /**
+     * 获取导航栏类型 或者 消息类型
+     * @param type
+     * @return
+     */
     @RequestMapping("/getContentType")
-    public List<Map<String,String>> getContentType(){
+    public List<Map<String,String>> getContentType(String type){
 
+        if("content".equals(type)){
 
-        List<Map<String,String>>result= menuService.getContentType(CommonConst.TYPE_NOTICEKIND);
-        return result;
+            List<Map<String,String>>result= menuService.getContentType(CommonConst.TYPE_NOTICEKIND);
+            return result;
+        }else if("menu".equals(type)){
+            List<Map<String,String>>result= new ArrayList<>();
+            HashMap<String, String> item1 = new HashMap<>();
+            item1.put("id",CommonConst.TYPE_NOTICEKIND);
+            item1.put("title","消息型导航栏");
+            result.add(item1);
+
+            HashMap<String, String> item2 = new HashMap<>();
+            item2.put("id",CommonConst.TYPE_INDEX);
+            item2.put("title","首页导航栏");
+            result.add(item2);
+
+            HashMap<String, String> item3 = new HashMap<>();
+            item3.put("id",CommonConst.TYPE_INDEX);
+            item3.put("title","自有内容导航栏");
+            result.add(item3);
+            return result;
+        }
+        return null;
     }
 }
